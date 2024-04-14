bce2bedcd046fd46f40f1acc57b480a7da08dda7
Akash Sharma <87300147+ak8459@users.noreply.github.com>
2024-04-14 23:28:36 +0530

    added .env file and improved css  for single movie page

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-// const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
-const TMDB_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNjRhM2I1NDY0ZmM5M2MyYTgwNDk2NmE3MWE3NDQ5ZCIsInN1YiI6IjY2MWE4MjYyNWZmMzRlMDE2MzU5MTdiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NghgLqIZrSlIf4t6PHSxv__ihSEeTSaM2DNa01avphc'
-
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
     Authorization: "bearer " + TMDB_TOKEN,
+    accept: 'application/json'
 };
  
 export const fetchDataFromApi = async (url, params) => {
